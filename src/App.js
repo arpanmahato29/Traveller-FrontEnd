283c46b5215d1da955fe2c2f760df22ef350edd1
Yashraj Jain <yashrajjain726@gmail.com>
2021-09-19 15:11:00 +0530

    hotel and flight card update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import HotelBooking from "./components/hotel_booking/HotelBooking";
 import Blogs from "./components/blogs/Blogs";
 import FlightResult from "./components/flight_result/FlightResult";
 import HotelResult from "./components/hotel_result/HotelResult";
-import Location from './components/places_to_visit/Location';
-import Destination from './components/places_to_visit/Destination';
+import Location from "./components/places_to_visit/Location";
+import Destination from "./components/places_to_visit/Destination";
 
 import "./assets/css/Carousal.css";
 import "./assets/css/image.css";
@@ -16,25 +16,42 @@ import "./assets/css/footer.css";
 import "./assets/css/color.css";
 import "./assets/css/booking_console.css";
 
-import './assets/css/Carousal.css';
-import './assets/css/image.css';
-import './assets/css/footer.css';
-import './assets/css/color.css';
-import './assets/css/booking_console.css'
-import './assets/css/text.css'
+import "./assets/css/Carousal.css";
+import "./assets/css/image.css";
+import "./assets/css/footer.css";
+import "./assets/css/color.css";
+import "./assets/css/booking_console.css";
+import "./assets/css/text.css";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/book_flight' component={FlightBooking} />
-          <Route exact path='/book_hotel' component={HotelBooking} />
-          <Route exact path='/blogs' component={Blogs} />
-          <Route exact path='/places_to_visit/:type/:location' component={Location} />
-          <Route exact path='/places_to_visit/:type/:location/:destination' component={Destination} />
-          <Route exact path='/book_flight/search_result' component={FlightResult} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/book_flight" component={FlightBooking} />
+          <Route exact path="/book_hotel" component={HotelBooking} />
+          <Route exact path="/blogs" component={Blogs} />
+          <Route
+            exact
+            path="/places_to_visit/:type/:location"
+            component={Location}
+          />
+          <Route
+            exact
+            path="/places_to_visit/:type/:location/:destination"
+            component={Destination}
+          />
+          <Route
+            exact
+            path="/book_flight/search_result"
+            component={FlightResult}
+          />
+          <Route
+            exact
+            path="/book_hotel/search_result"
+            component={HotelResult}
+          />
         </Switch>
       </BrowserRouter>
     </>
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,59 +1,94 @@
-import React from 'react';
-import {Link, withRouter} from 'react-router-dom'
-import {HiOutlinePencil} from 'react-icons/hi';
-import {RiHeart2Line} from 'react-icons/ri';
-import {VscBellDot} from 'react-icons/vsc';
-import {FaSignInAlt} from 'react-icons/fa';
-import {GiTripleGate} from 'react-icons/gi';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import { HiOutlinePencil } from "react-icons/hi";
+import { RiHeart2Line } from "react-icons/ri";
+import { VscBellDot } from "react-icons/vsc";
+import { FaSignInAlt } from "react-icons/fa";
+import { GiTripleGate } from "react-icons/gi";
 
 const currentTab = (history, path) => {
-  if(history.location.pathname === path){
-    return { color: '#198754'}
+  if (history.location.pathname === path) {
+    return { color: "#198754" };
   } else {
-    return {color: '#adb5bd'}
+    return { color: "#adb5bd" };
   }
-}
+};
 
-const Header = ({history}) => {
-  return(
-      <header>
-        <div className="row">
-          <div className="col-1"></div>
-          <div className="col-10">
-            <nav className="navbar navbar-expand-lg navbar-light">
-              <div className="container-fluid">
-                <a className="navbar-brand text-success" href="/">
-                  <GiTripleGate size={42} className=" me-2 mb-2 pb-2"/>
-                  <h1 className="d-inline-block fw-bold ">Traveller</h1>
-                </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                  <div className="d-flex">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                      <li className="nav-item me-2">
-                        <Link className="nav-link" to='/blogs' style={currentTab(history,'/blogs')}><h6 className="m-auto mb-2"><HiOutlinePencil className="me-1" size={20}/>Blogs</h6></Link>
-                      </li>
-                      <li className="nav-item me-2">
-                        <a className="nav-link" href="#"><h6 className="m-auto"><RiHeart2Line className="me-1" size={20}/>Trips</h6></a>
-                      </li>
-                      <li className="nav-item me-2">
-                        <a className="nav-link" href="#"><h6 className="m-auto"><VscBellDot className="me-1" size={20}/>Alerts</h6></a>
-                      </li>
-                      <li class="nav-item me-2">
-                        <a class="nav-link btn btn-dark text-light px-2"><h6 className="m-auto"><FaSignInAlt className="me-1" size={20}/>Sign in</h6></a>
-                      </li>
-                    </ul>
-                  </div>
+const Header = ({ history }) => {
+  return (
+    <header>
+      <div className="row">
+        <div className="col-1"></div>
+        <div className="col-10">
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <div className="container-fluid">
+              <a className="navbar-brand text-success" href="/">
+                <GiTripleGate size={42} className=" me-2 mb-2 pb-2" />
+                <h1 className="d-inline-block fw-bold ">Traveller</h1>
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div
+                className="collapse navbar-collapse justify-content-end"
+                id="navbarSupportedContent"
+              >
+                <div className="d-flex">
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item me-2">
+                      <Link
+                        className="nav-link"
+                        to="/blogs"
+                        style={currentTab(history, "/blogs")}
+                      >
+                        <h6 className="m-auto mb-2">
+                          <HiOutlinePencil className="me-1" size={20} />
+                          Blogs
+                        </h6>
+                      </Link>
+                    </li>
+                    <li className="nav-item me-2">
+                      <a className="nav-link" href="#">
+                        <h6 className="m-auto">
+                          <RiHeart2Line className="me-1" size={20} />
+                          Trips
+                        </h6>
+                      </a>
+                    </li>
+                    <li className="nav-item me-2">
+                      <a className="nav-link" href="#">
+                        <h6 className="m-auto">
+                          <VscBellDot className="me-1" size={20} />
+                          Alerts
+                        </h6>
+                      </a>
+                    </li>
+                    <li className="nav-item me-2">
+                      <a className="nav-link btn btn-dark text-light px-2">
+                        <h6 className="m-auto">
+                          <FaSignInAlt className="me-1" size={20} />
+                          Sign in
+                        </h6>
+                      </a>
+                    </li>
+                  </ul>
                 </div>
               </div>
-            </nav>
-          </div>
-          <div class="col-1"></div>
+            </div>
+          </nav>
         </div>
-      </header>
+        <div className="col-1"></div>
+      </div>
+    </header>
   );
-}
+};
 
 export default withRouter(Header);
